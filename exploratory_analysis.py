8cae15633e3437ab7f1b46a2454d15ca7d393a3d
Aidan Nagola <115124316+anago001@users.noreply.github.com>
2026-03-16 05:15:39 -0700

    Add summarize_variable function for summary statistics

--- exploratory_analysis.py
+++ exploratory_analysis.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+#Summarize Variables Function
 def summarize_variable(df, column):
     summary = df[column].describe()
     print("Summary Statistics:")
